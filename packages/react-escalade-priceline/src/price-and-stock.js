334b1b2a5cceac1c35a612711d347226467be818
Taylor <tbaustin1992@gmail.com>
2018-04-11 08:24:37 -0500

    conflict

--- packages/react-escalade-priceline/src/price-and-stock.js
+++ packages/react-escalade-priceline/src/price-and-stock.js
@@ -3,9 +3,9 @@ import { Subscribe } from 'statable';
 
 import { Stock, Price } from './';
 
-<<<<<<< HEAD
 class PriceAndStock extends Component {
   render() {
+    const renderChild = this.props.children;
     return (
       <Stock {...this.props}>
         {({ stock, loading }) => {
@@ -13,9 +13,11 @@ class PriceAndStock extends Component {
           return (
             <Price {...this.props}>
               {({ price, loading }) => {
-                if (!this.props.children) return null;
+                if (!renderChild) {
+                  return null;
+                }
                 let priceLoading = loading;
-                return this.props.children({
+                return renderChild({
                   stock,
                   stockLoading,
                   price,
@@ -29,36 +31,6 @@ class PriceAndStock extends Component {
       </Stock>
     );
   }
-=======
-class PriceAndStock extends Component{
-	render() {
-		const renderChild = this.props.children
-		return (
-			<Stock {...this.props}>
-				{({ stock, loading }) => {
-					let stockLoading = loading
-					return (
-						<Price {...this.props}>
-							{({ price, loading }) => {
-								if (!renderChild){
-									return null
-								}
-								let priceLoading = loading
-								return renderChild({
-									stock,
-									stockLoading,
-									price,
-									priceLoading,
-									loading: stockLoading || priceLoading,
-								})
-							}}
-						</Price>
-					)
-				}}
-			</Stock>
-		)
-	}
->>>>>>> 161155565ef49ec63110a2c7789ec9e6428cdf8c
 }
 
 export default PriceAndStock;
