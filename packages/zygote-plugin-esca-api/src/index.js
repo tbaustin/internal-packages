38d8c7726f5b5539b92d5192ef4bb2fb341d24bc
Matt Suhay <matthew.suhay@gmail.com>
2019-04-23 14:16:51 -0500

    Adding error correction

--- packages/zygote-plugin-esca-api/src/index.js
+++ packages/zygote-plugin-esca-api/src/index.js
@@ -8,13 +8,15 @@
 
 // export default Api
 
-const existingScript = document.getElementById('ployfill')
+if (typeof window !== 'undefined') {
+	const existingScript = document.getElementById('polyfill')
 
-if (!existingScript) {
-	const script = document.createElement('script')
-	script.src = 'https://polyfill.io/v3/polyfill.min.js?features=String.prototype.startsWith'
-	script.id = 'ployfill'
-	document.body.appendChild(script)
+	if (!existingScript) {
+		const script = document.createElement('script')
+		script.src = 'https://polyfill.io/v3/polyfill.min.js?features=String.prototype.startsWith'
+		script.id = 'polyfill'
+		document.body.appendChild(script)
+	}
 }
 
 export * from './info'
