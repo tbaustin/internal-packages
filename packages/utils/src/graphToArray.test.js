0b6001913735e45b3aa347f038182908219a1e48
tbaustin <tbaustin1992@gmail.com>
2020-03-18 14:02:43 -0500

    start on utils

--- packages/utils/src/graphToArray.test.js
+++ packages/utils/src/graphToArray.test.js
@@ -2,42 +2,42 @@ import { graphToArray } from './index'
 
 
 const graph1 = {
-  edges: [
-    { node: `test1` },
-    { node: `test2` },
-    { node: `test3` }
-  ]
+	edges: [
+		{ node: `test1` },
+		{ node: `test2` },
+		{ node: `test3` },
+	],
 }
 
 const graph2 = {
-  edges: [
-    {
-      node: {
-        frontmatter: {
-          firstName: `Spike`,
-          lastName: `Spiegel`
-        }
-      }
-    },
-    {
-      node: {
-        frontmatter: {
-          firstName: `Faye`,
-          lastName: `Valentine`
-        }
-      }
-    },
-  ]
+	edges: [
+		{
+			node: {
+				frontmatter: {
+					firstName: `Spike`,
+					lastName: `Spiegel`,
+				},
+			},
+		},
+		{
+			node: {
+				frontmatter: {
+					firstName: `Faye`,
+					lastName: `Valentine`,
+				},
+			},
+		},
+	],
 }
 
 const graph3 = {
-  edges: [{
-    node: {
-      frontmatter: {
-        people: [{ name: `Jet Black` }]
-      }
-    }
-  }]
+	edges: [{
+		node: {
+			frontmatter: {
+				people: [{ name: `Jet Black` }],
+			},
+		},
+	}],
 }
 
 
@@ -46,24 +46,24 @@ const description = `Converts a graph structure to an Array containing `
 
 
 test(description, () => {
-  const result1 = graphToArray(graph1)
-  expect(result1).toBeInstanceOf(Array)
-  expect(result1).toHaveLength(3)
+	const result1 = graphToArray(graph1)
+	expect(result1).toBeInstanceOf(Array)
+	expect(result1).toHaveLength(3)
 
-  const result2 = graphToArray(graph2, `frontmatter`)
-  expect(result2).toBeInstanceOf(Array)
-  expect(result2).toHaveLength(2)
-  expect(result2[0]).toMatchObject({
-    firstName: `Spike`,
-    lastName: `Spiegel`
-  })
+	const result2 = graphToArray(graph2, `frontmatter`)
+	expect(result2).toBeInstanceOf(Array)
+	expect(result2).toHaveLength(2)
+	expect(result2[0]).toMatchObject({
+		firstName: `Spike`,
+		lastName: `Spiegel`,
+	})
 
-  const result3 = graphToArray(graph3, `frontmatter.people[0].name`)
-  expect(result3).toBeInstanceOf(Array)
-  expect(result3).toHaveLength(1)
-  expect(result3[0]).toBe(`Jet Black`)
+	const result3 = graphToArray(graph3, `frontmatter.people[0].name`)
+	expect(result3).toBeInstanceOf(Array)
+	expect(result3).toHaveLength(1)
+	expect(result3[0]).toBe(`Jet Black`)
 
-  const result4 = graphToArray(null, `test`)
-  expect(result4).toBeInstanceOf(Array)
-  expect(result4).toHaveLength(0)
+	const result4 = graphToArray(null, `test`)
+	expect(result4).toBeInstanceOf(Array)
+	expect(result4).toHaveLength(0)
 })
--- packages/utils/src/index.js
+++ packages/utils/src/index.js
@@ -5,12 +5,12 @@ import _get from 'lodash/get'
  * Converts a graph structure to an Array containing the graph's original nodes
  */
 export function graphToArray(graph, path) {
-  const { edges = [] } = graph || {}
+	const { edges = [] } = graph || {}
 
-  return edges.map(edge => {
-    const { node } = edge || {}
-    return path ? _get(node, path) : node
-  })
+	return edges.map(edge => {
+		const { node } = edge || {}
+		return path ? _get(node, path) : node
+	})
 }
 
 
@@ -18,6 +18,27 @@ export function graphToArray(graph, path) {
  * Gets the display name of a React component
  */
 export function getComponentName(component) {
-  const { displayName, name } = component || {}
+	const { displayName, name } = component || {}
 	return displayName || name || `Component`
 }
+
+/**
+ * Return formatted price: 2, `2.3` => `$2.00`, `$2.30`
+ */
+
+export function formatPrice(price) {
+	console.log(price)
+}
+
+/**
+ * Converts dollars to cents: $3.00 => 300 cents
+ */
+
+export function toCents(value) {
+	console.log(value)
+}
+
+/**
+ * Converts cents to dollars: 300 cents => $3.00
+ */
+
