ceae98cd41037fb39fb733ab176d16fddf29fcdf
Amado <amadoa@gmail.com>
2020-07-30 14:52:30 -0400

    add item location to each item on loading products

--- packages/zygote-plugin-esca-api/src/postInfo/loadProducts.js
+++ packages/zygote-plugin-esca-api/src/postInfo/loadProducts.js
@@ -2,13 +2,30 @@ const  loadProducts = async (skus, products, callback) => {
 	//Get coupon from the API
 	let returnAsObject = true
 	var response = await callback({
-		fields: [`price`,`shipping`],
+		fields: [`inventory`,`price`,`shipping`],
 		...skus,
 		returnAsObject,
 	})
 	//Add Quantity to products response
 	products.forEach(product => {
-		response[product.id.toUpperCase()].qty = product.quantity
+		const sku = product.id.toUpperCase()
+		response[sku].qty = product.quantity
+		//find location with most quantity or default to 1st
+		const locations = Object.entries(response[sku].all_stocks)
+		console.log(`locations entries`, locations)
+		const itemLocation = locations.reduce((location, currentLocation) => {
+			const [, val] = location
+			const [, curVal] = currentLocation
+			if(curVal > val) return currentLocation
+			return location
+		}, locations[0])
+
+		console.log(`location reduced`, itemLocation)
+		const [name, val] = itemLocation
+		response[sku].location = {
+			[name]: val,
+		}
+		console.log(`load products response`, response)
 	})
 	
 	return response
