e1c2212babb9f4ae26c2d226b08bac285bd7690e
MastaAaron <a.resing@hotmail.com>
2020-08-17 17:16:18 -0500

    Gatsby theme: update AC form widget for new config structure

--- packages/gatsby-theme-esca-boilerplate/src/widgets/ActiveCampaignWidget.js
+++ packages/gatsby-theme-esca-boilerplate/src/widgets/ActiveCampaignWidget.js
@@ -1,27 +1,26 @@
 import React, { useEffect } from 'react'
+import { activeCampaign } from 'config'
 
-import { activeCampaignSite } from 'config'
+const { account } = activeCampaign
+
 
 export default function ActiveCampaignWidget(props) {
 	const { formId } = props
-  
-  
+
 	useEffect(() => {
 		const formScript = document.createElement(`script`)
-		formScript.src = `https://${activeCampaignSite}.activehosted.com/f/embed.php?id=${formId}`
+		formScript.src = `https://${account}.activehosted.com/f/embed.php?id=${formId}`
 		formScript.type = `text/javascript`
 		formScript.charset = `utf-8`
 
 		document.head.appendChild(formScript)
-      
+
 		return () => {
 			formScript.parentNode.removeChild(formScript)
 		}
 	})
 
-	if(formId){
-		return <div className={`_form_${formId}`} />
-	} else {
-		return <div>Form Id must be provided</div>
-	}
+	return formId
+		?	<div className={`_form_${formId}`} />
+		: <div>Form ID must be provided.</div>
 }
