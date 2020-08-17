ed12c34b38954ed7d8fb0a6a45aef00c7a117e25
MastaAaron <a.resing@hotmail.com>
2020-08-17 17:17:07 -0500

    Gatsby theme: upgrade cart, add ActiveCampaign plugin

--- packages/gatsby-theme-esca-boilerplate/src/components/cart.js
+++ packages/gatsby-theme-esca-boilerplate/src/components/cart.js
@@ -1,22 +1,81 @@
-import React from 'react'
-import { Cart as ZygoteCart } from '@escaladesports/zygote-cart'
-import * as escaApi from '@escaladesports/zygote-plugin-esca-api'
-import * as standardPayment from '@escaladesports/zygote-cart/dist/plugins/zygote-plugin-standard-billing'
+import React, { useEffect } from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
+import { Cart as Zygote, standardPayment } from '@escaladesports/zygote-cart'
+import * as zygoteActiveCampaign from 'zygote-plugin-active-campaign'
+import * as zygoteEscaApi from '@escaladesports/zygote-plugin-esca-api'
+import { activeCampaign as acConfig } from 'config'
+
+
+const query = graphql`
+	query CartQuery {
+		siteSettings: sanitySiteSettings {
+			title
+		}
+	}
+`
+
+const reCaptchaKey = process.env.GATSBY_CART_RECAPTCHA_KEY
+const reCaptchaScoreThreshold = process.env.GATSBY_CART_RECAPTCHA_THRESHOLD
+const reCaptchaProps = !reCaptchaKey ? {} : {
+	reCaptchaKey, reCaptchaScoreThreshold,
+}
 
 
 export default function Cart() {
+	const { siteSettings } = useStaticQuery(query)
+	const cartHeaderText = siteSettings?.title || `Escalade Sports`
+
+	// Initialize ActiveCampaign functionality on 1st render
+	useEffect(() => {
+		zygoteActiveCampaign.init(
+			{
+				serviceName: acConfig.integrationName,
+				serviceLogoUrl: acConfig.integrationLogo,
+				proxyUrl: `${acConfig.origin}/api/3/`,
+				origin: acConfig.origin,
+				host: acConfig.host
+			},
+			{
+				proxyDevUrl: `${acConfig.devOrigin}/dev/api/3/`,
+				devOrigin: acConfig.devOrigin,
+				isDevMode: acConfig.devMode,
+				isLogging: acConfig.devMode
+			},
+			{
+				acceptsMarketing: true,
+				color: `#000000`,
+				text: `I would like to receive emails and updates about my order and`
+					+	` special promotions`,
+				hasFullImageUrl: true,
+				addAbandonedTag: true
+			},
+			{
+				abandonOffset: 30
+			},
+			{
+				// This will clear the contact from the automations
+				clearAutomations: true
+			}
+		)
+	}, [])
+
 	return (
-		<ZygoteCart
+		<Zygote
 			styles={{
 				borderColor: `#c8202e`,
 				primaryColor: `#c8202e`,
 				overlayColor: `rgba(200, 32, 46, .5)`,
 			}}
-			header={<h1>Lifeline Fitness</h1>}
+			header={<h1>{cartHeaderText}</h1>}
 			cartHeader={<div>Shopping Cart</div>}
-			infoWebhook='/api/inventory/load'
+			infoWebhook="/api/inventory/load"
 			splitName={true}
-			plugins={[ standardPayment, escaApi ]}
+			plugins={[
+				standardPayment,
+				zygoteEscaApi,
+				zygoteActiveCampaign
+			]}
+			{...reCaptchaProps}
 			totalModifications={[
 				{
 					id: `shipping`,
