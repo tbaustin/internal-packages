5d473c76218c24b56ff53d37e1c8df0751b05e9f
MastaAaron <a.resing@hotmail.com>
2020-09-14 16:14:23 -0500

    set test-site to use correct Sanity project/dataset

--- packages/test-site/config.js
+++ packages/test-site/config.js
@@ -2,8 +2,8 @@ module.exports = {
 	escaladeSite: `lifeline`,
 	siteId: `escalade-website-boilerplate`,
 	sanityName: `Boilerplate`,
-	sanityProjectId: `c3g4r239`, // change this
-	sanityDataset: `dev-aaron`,
+	sanityProjectId: `f25ydn7y`,
+	sanityDataset: `prod`,
 	googleTagManagerId: `GTM-THRMFMP`,
 	activeCampaign: {
 		integrationName: `Boilerplate Test Site`,
