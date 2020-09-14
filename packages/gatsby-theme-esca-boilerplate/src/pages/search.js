55b9ffa69b52046ccb90123f95fb0ab85a82ef02
MastaAaron <a.resing@hotmail.com>
2020-09-14 09:35:21 -0500

    refactor & further improve behavior of search page, add submit button

--- packages/gatsby-theme-esca-boilerplate/src/pages/search.js
+++ packages/gatsby-theme-esca-boilerplate/src/pages/search.js
@@ -3,49 +3,93 @@ import Link from 'gatsby-link'
 import { css } from '@emotion/core'
 import Layout from '../layout'
 import Container from '../components/container'
+import CallToAction from '../components/call-to-action'
 import { search } from '../search'
 import { colors, breakpoints } from '../styles/variables'
 
 
 
-export default function SearchPage(){
+const ResultsDisplay = props => {
+	const { loading, results, submittedTerm } = props
+	const hasResults = Boolean(results?.length)
+
+	if (loading) return (
+		<h3>
+			Searching for <em>"{submittedTerm}"</em>...
+		</h3>
+	)
+
+	if (hasResults) return (
+		<>
+			<h3>
+				Results for <em>"{submittedTerm}"</em>
+			</h3>
+			<ul css={searchListCss}>
+				{results.slice(0, 10).map(({ salsify, path, sku }, index) => {
+					return (
+						<li className={`searchItem`} key={`searchResult${index}`}>
+							<Link className={`result`} to={path || `/product/${sku}`}>
+								{salsify?.[`Item Name`] || sku}
+							</Link>
+						</li>
+					)
+				})}
+			</ul>
+		</>
+	)
+
+	if (submittedTerm) return (
+		<h3>
+			No results found for <em>"{submittedTerm}"</em>
+		</h3>
+	)
+
+	return null
+}
+
+
+
+export default function SearchPage() {
 	const [loading, setLoading] = useState(false)
 	const [results, setResults] = useState([])
 	const [term, setTerm] = useState(``)
+	const [submittedTerm, setSubmittedTerm] = useState(``)
 
 
-	const startSearch = async (value) => {
-		setTerm(value)
+	const startSearch = async (customTerm = term) => {
+		// Save the term that is being searched for display purposes
+		setSubmittedTerm(customTerm)
 
-		// Change URL
+		// Visually update URL
 		if (window?.history?.replaceState) {
-			let path = encodeURIComponent(value).replace(/%20/g, `+`)
+			let path = encodeURIComponent(customTerm).replace(/%20/g, `+`)
 			window.history.replaceState({}, ``, `/search/${path}`)
 		}
 
 		setLoading(true)
-		const searchResults = await search(value)
+		const searchResults = await search(customTerm)
 		setLoading(false)
 		setResults(searchResults)
 	}
 
 
-	const handleInput = event => {
+	const handleInputEvent = event => {
 		const { type, key, currentTarget } = event
-		if (key === `Enter` || type === `blur`) {
-			startSearch(currentTarget?.value || ``)
-		}
+		setTerm(currentTarget?.value || ``)
+		if (key === `Enter`) startSearch()
 	}
+
 
 	useEffect(() => {
 		// Check URL for search term
 		const path = document.location.pathname.split(`/`)
-		if (path.length === 3) {
-			let term = path.pop()
-			term = term.replace(/\+/g, `%20`)
-			term = decodeURIComponent(term)
-			startSearch(term)
-		}
+		if (path.length !== 3) return
+
+		// Set initial search term from URL parameter if found
+		let urlTerm = decodeURIComponent(
+			path.pop().replace(/\+/g, `%20`)
+		)
+		startSearch(urlTerm)
 	}, [])
 
 
@@ -55,31 +99,22 @@ export default function SearchPage(){
 				<h1>Search</h1>
 				<input
 					type="text"
+					placeholder="Enter a new search term here"
 					css={searchInputStyle}
 					disabled={loading}
-					onKeyUp={handleInput}
-					onBlur={handleInput}
+					value={term}
+					onChange={handleInputEvent}
+					onKeyUp={handleInputEvent}
 				/>
-				{loading && (
-					<h3>Searching for <em>"{term}"</em>...</h3>
-				)}
-				{term && !results.length && !loading && (
-					<h3>No results found for <em>"{term}"</em></h3>
-				)}
-				{!!results.length && <>
-					<h3>Results for <em>"{term}"</em></h3>
-					<ul css={searchListCss}>
-						{results.slice(0, 10).map(({ salsify, path, sku }, index) => {
-							return (
-								<li className={`searchItem`} key={`searchResult${index}`}>
-									<Link className={`result`} to={path || `/product/${sku}`}>
-										{salsify?.[`Item Name`] || sku}
-									</Link>
-								</li>
-							)
-						})}
-					</ul>
-				</>}
+				<CallToAction
+					text="submit"
+					onClick={() => startSearch()}
+				/>
+				<ResultsDisplay
+					loading={loading}
+					submittedTerm={submittedTerm}
+					results={results}
+				/>
 			</Container>
 		</Layout>
 	)
@@ -88,9 +123,10 @@ export default function SearchPage(){
 
 
 const searchInputStyle = css`
-	padding: 0.5rem;
+	padding: 0.75rem;
 	font-size: 1.25rem;
 	border-radius: 4px;
+	flex: 1;
 `
 
 
