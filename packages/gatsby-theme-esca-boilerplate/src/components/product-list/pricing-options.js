0d5e1c7a40f7303d49ec2a3bc6003cc67957cb34
tbaustin <tbaustin1992@gmail.com>
2020-09-23 16:11:13 -0500

    work on product tile

--- packages/gatsby-theme-esca-boilerplate/src/components/product-list/pricing-options.js
+++ packages/gatsby-theme-esca-boilerplate/src/components/product-list/pricing-options.js
@@ -1,7 +1,16 @@
 export default function(variants) {
 	const prices = variants?.map?.(v => v?.price)
+	const salePrices = variants?.reduce?.((acc, cur) => {
+		const { customFieldEntries } = cur
+		const salePrice = customFieldEntries?.find?.(f =>  f.fieldName === `Sale Price`)?.fieldValue?.number
+		
+		return salePrice ? [...acc, +salePrice] : acc
+	}, [])
+
 	const min = Math.min(...prices)
 	const max = Math.max(...prices)
+	const minSale = salePrices.length ? Math.min(...salePrices) : 0
+	const maxSale = salePrices.length ? Math.max(...salePrices) : 0
 	const defaultPrice = prices?.[0]
 
 	return {
@@ -9,5 +18,6 @@ export default function(variants) {
 		priceRange: min < max ? [min, max] : defaultPrice,
 		priceLowest: min,
 		priceHighest: max,
+		salePriceRange: maxSale > max && minSale > min ? [minSale, maxSale] : null,
 	}
 }
--- packages/gatsby-theme-esca-boilerplate/src/components/product-list/product-tile.js
+++ packages/gatsby-theme-esca-boilerplate/src/components/product-list/product-tile.js
@@ -4,10 +4,14 @@ import { useStaticQuery, Link, graphql } from 'gatsby'
 import { formatPrice, getFluidGatsbyImage } from '@escaladesports/utils'
 import { css } from '@emotion/core'
 
+import ReviewSnippet from '../../widgets/ReviewSnippetWidget'
+
 import { useTemplateEngine } from '../../context/template-engine'
 import pricingOptions from './pricing-options'
 
 import { priceText } from '../../styles'
+
+import { colors } from '../../styles/variables'
 
 const imageKeyQuery = graphql`{
   imageCustomField: sanityCustomField(useAsListImage: { eq: true }) {
@@ -16,10 +20,19 @@ const imageKeyQuery = graphql`{
 }`
 
 export default function ProductTile({ product, priceDisplay }){
-	const { variants, sku  } = product
+	console.log(`Product: `, product)
+	const { variants, sku, customFieldEntries, salsify  } = product
+	const { Brand } = salsify
+
 	const defaultVariant = variants?.[0]
 
-	const price = pricingOptions(variants)?.[priceDisplay]
+	const inStock = variants?.some?.(v => !!v.stock)
+
+	const priceConfig = pricingOptions(variants)
+	const price = priceConfig?.[priceDisplay]
+
+	const salePriceRange = priceConfig?.salePriceRange
+	const salePrice = customFieldEntries?.find?.(f =>  f.fieldName === `Sale Price`)?.fieldValue?.number
 
 	const { imageCustomField } = useStaticQuery(imageKeyQuery)
 
@@ -56,13 +69,43 @@ export default function ProductTile({ product, priceDisplay }){
 					? <Img fluid={image} />
 					: <img src="https://via.placeholder.com/400" alt="placeholder" />
 			}
-			<div className="price">
+
+			{!!variants && <div className="hasVariants smallText">More Options Available</div>}
+
+			<strong>{patchedData.name}</strong>
+
+			{!!Brand && <div className="brand smallText">by {Brand}</div>}
+
+			<div className="pricing">
 				{Array.isArray(price)
-					? `${formatPrice(price[0])} - ${formatPrice(price[1])}`
-					: formatPrice(price)
+					? (
+						<>
+							<div className="price range" css={css`
+								margin-bottom: ${salePriceRange ? `10px` : `0px`};
+							`}>
+								{formatPrice(price[0])} - {formatPrice(price[1])}
+							</div>
+							{salePriceRange && (
+								<div className="salePrice range">
+									{formatPrice(salePriceRange[0])} - {formatPrice(salePriceRange[1])}
+								</div>
+							)}
+						</>
+					)
+					: (
+						<>
+							<div className="price">{formatPrice(price)}</div>
+							{salePrice && (<div className="salePrice">{formatPrice(salePrice)}</div>)}
+						</>
+					)
 				}
 			</div>
-			<strong>{patchedData.name}</strong>
+
+			<div className="stock smallText">{inStock ? `In Stock` : `Out of Stock`}</div>
+
+			<div className="snippet smallText">
+				<ReviewSnippet sku={sku} />
+			</div>
 		</Wrapper>
 	)
 }
@@ -73,12 +116,32 @@ const productTileStyles = css`
   justify-content: flex-start;
   align-items: center;
   padding: 0 10px;
-	text-align: left;
+	text-align: center;
   color: black !important;
   text-decoration: none;
-
+	transition: all 0.2s ease-in-out 0s;
+	:hover {
+		transform: scale(1.015);
+		box-shadow: rgb(51, 51, 51) 0px 10px 15px -10px;
+	}
+	.snippet {
+		.reviewSnippetWidget {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			.star {
+				width: 20px;
+				height: 20px;
+			}
+		}
+	}
+	.smallText {
+		color: ${colors.textMedium};
+		font-size: .75em;
+	}
   > * {
-    margin-bottom: 20px;
+    margin-bottom: 10px;
+		width: 100%;
   }
 	.gatsby-image-wrapper {
 		width: 100%;
@@ -90,11 +153,28 @@ const productTileStyles = css`
     object-fit: contain;
   }
   strong {
-    font-size: 1.5rem;
+    font-size: 1.1rem;
 		align-self: flex-start;
   }
+	.pricing {
+		align-self: flex-start;
+		display: flex;
+		flex-flow: wrap row;
+		align-items: center;
+		justify-content: center;
+	}
 	.price {
 		${priceText}
-		align-self: flex-start;
+		&:not(.range) {
+			margin-right: 10px;
+		}
+	}
+	.range {
+		flex-basis: 100%;
+	}
+	.salePrice {
+		color: ${colors.textDark};
+		font-size: 1em;
+		text-decoration: line-through;
 	}
 `
