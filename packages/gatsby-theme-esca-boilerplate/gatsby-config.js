d6ca0a4b99cd5d8d2ee4c0a744d9b7e8c11ce675
tbaustin <tbaustin1992@gmail.com>
2020-09-25 10:46:41 -0500

    try add force slashes

--- packages/gatsby-theme-esca-boilerplate/gatsby-config.js
+++ packages/gatsby-theme-esca-boilerplate/gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = ({ icon }) => ({
 		siteUrl,
 	},
 	plugins: [
-		`gatsby-plugin-remove-trailing-slashes`,
+		`gatsby-plugin-force-trailing-slashes`,
 		`gatsby-plugin-react-helmet`,
 		`gatsby-plugin-sitemap`,
 		{
