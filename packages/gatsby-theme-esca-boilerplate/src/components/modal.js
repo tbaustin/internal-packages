be70d819814945531d52e43ad122f2569c044adf
MastaAaron <a.resing@hotmail.com>
2020-10-07 16:58:36 -0500

    slight refactor of Modal component, implement prop to disable padding

--- packages/gatsby-theme-esca-boilerplate/src/components/modal.js
+++ packages/gatsby-theme-esca-boilerplate/src/components/modal.js
@@ -1,29 +1,38 @@
 import React from 'react'
 import { css } from '@emotion/core'
 
-export default function Modal(props) {
-	const { active, children } = props
 
-	return ( 
-		<div css={styles} className={`modal ${active ? `activeModal` : ``}`}>
+export default function Modal(props) {
+	const { active, children, noPadding } = props
+
+	const styles = getStyles(props)
+
+	return (
+		<div css={styles}>
 			{children}
 		</div>
 	)
 }
 
-const styles = css`
-  display: none;
-  position: fixed;
-  top: 0;
-  left: 0;
-  height: 100vh;
-  width: 100%;
-  background: rgba(0, 0, 0, .5);
-  justify-content: center;
-  align-items: flex-start;
-  z-index: 2000;
-  padding: 50px;
-  &.activeModal {
-    display: flex;
-  }
-` 
+
+const getStyles = props => {
+	const { active, noPadding } = props
+
+	const display = active ? `flex` : `none`
+	const padding = noPadding ? `0` : `50px`
+
+	return css`
+	  display: ${display};
+	  position: fixed;
+	  top: 0;
+	  left: 0;
+		bottom: 0;
+		right: 0;
+
+	  background: rgba(0, 0, 0, .5);
+	  justify-content: center;
+	  align-items: flex-start;
+	  z-index: 2000;
+	  padding: ${padding};
+	`
+}
