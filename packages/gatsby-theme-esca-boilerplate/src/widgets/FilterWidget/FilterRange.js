80f48fd5d6e69bc3b4647eb9cd3de28a3e1db1ab
MastaAaron <a.resing@hotmail.com>
2020-10-07 18:20:28 -0500

    slightly refactor & visually polish range filter

--- packages/gatsby-theme-esca-boilerplate/src/widgets/FilterWidget/FilterRange.js
+++ packages/gatsby-theme-esca-boilerplate/src/widgets/FilterWidget/FilterRange.js
@@ -1,6 +1,10 @@
 import React, { useState } from 'react'
 import { css } from '@emotion/core'
-import produce from 'immer'
+import { breakpoints } from '../../styles/variables'
+
+
+const initialInputValues = { min: ``, max: `` }
+
 
 export default function FilterRange(props){
 	const {
@@ -11,83 +15,96 @@ export default function FilterRange(props){
 		placeholderTo
 	} = props
 
-	const [range, setRange] = useState({ min: `0`, max: `0` })
+	const [range, setRange] = useState(initialInputValues)
 
-	function addFilter(e) {
+	const handleInput = e => {
+		const { name, value } = e.target
+		setRange({ ...range, [name]: value })
+	}
+
+	const addFilter = e => {
 		e.preventDefault()
-		setActiveFilters({
-			...activeFilters,
-			[title]: range,
-		})
+		setActiveFilters({ ...activeFilters, [title]: range })
 	}
 
 	const removeFilter = () => {
-		const activeFiltersCopy = produce(activeFilters, draft => {
-			delete draft[title]
-		})
-		setActiveFilters(activeFiltersCopy)
+		const newActiveFilters = { ...activeFilters }
+		delete newActiveFilters[title]
+		setActiveFilters(newActiveFilters)
+		setRange(initialInputValues)
 	}
 
 	return (
 		<div css={styles}>
 			<div className="inputs">
-				<div className="min">
-					<input
-						type="number"
-						placeholder={placeholderFrom || `Min`}
-						value={range.min}
-						onChange={e => setRange({...range, min: e.target.value})}
-					/>
-				</div>
-				to
-				<div className="max">
-					<input
-						type="number"
-						placeholder={placeholderTo || `Max`}
-						value={range.max}
-						onChange={e => setRange({...range, max: e.target.value})}
-					/>
-				</div>
-				<div className="setPrice">
-					<button onClick={addFilter}>Go</button>
-				</div>
+				<input
+					name="min"
+					type="number"
+					placeholder={placeholderFrom || `Min`}
+					value={range.min}
+					onChange={handleInput}
+				/>
+				<span>to</span>
+				<input
+					name="max"
+					type="number"
+					placeholder={placeholderTo || `Max`}
+					value={range.max}
+					onChange={handleInput}
+				/>
+				<button className="setPrice" onClick={addFilter}>
+					Go
+				</button>
 			</div>
 			{title in activeFilters && (
-				<div className="remove">
-					<button onClick={removeFilter}>
-						Remove
-					</button>
-				</div>
+				<button className="remove" onClick={removeFilter}>
+					Clear
+				</button>
 			)}
 		</div>
 	)
 }
 
+
 const styles = css`
 	margin-bottom: 20px;
+
 	.remove {
-		margin: 10px;
-		display: flex;
-		justify-content: center;
-		button {
-			outline: none;
-			cursor: pointer;
-		}
+		margin-top: 0.75rem;
+		padding: 0.5rem;
+		cursor: pointer;
 	}
+
 	.inputs {
 		display: flex;
-		justify-content: space-evenly;
+		align-items: center;
+
 		input {
-			width: 65px;
-			padding: 6px;
-			text-align: center;
+			width: 4.5rem;
+			padding: 0.75rem;
+		}
+
+		input, span {
+			margin-right: 0.75rem;
 		}
 	}
+
 	.setPrice {
-		button {
-			height: 100%;
-			padding: 0 10px;
-			cursor: pointer;
+		width: 4rem;
+		height: 100%;
+		padding: 0.75rem;
+		cursor: pointer;
+	}
+
+	@media(${breakpoints.laptop}) {
+		.inputs input {
+			width: 4rem;
+			padding: 0.5rem;
+		}
+
+		.setPrice {
+			width: 3rem;
+			padding: 0.5rem;
 		}
 	}
 `
